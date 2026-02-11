96052308991e4c5bc2694b05fff51f749ea15624
albedo121 <vyzzal999@gmail.com>
2026-02-12 00:43:57 +0530

    Added Dragdrop file and completed assignment 5

---
+++ src/test/java/Assignment5.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class Assignment5 {
+    public static void main(String[] args) {
+
+        //Initial Setup
+        WebDriver driver = new ChromeDriver();  //Open chrome driver
+        driver.manage().window().maximize();  //Maximize window
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));  //Implicit wait of 10s
+
+        //SCRIPT BEGINS FROM HERE--------------------------------------------------------
+        String url = "https://the-internet.herokuapp.com/";
+        driver.get(url);
+
+        driver.findElement(By.xpath("//a[text()='Nested Frames']")).click();  //Find Nested Frames link and click
+
+        WebElement frame_top = driver.findElement(By.cssSelector("frame[name='frame-top']"));   //Find Top frame
+        driver.switchTo().frame(frame_top);  //Switch to top frame
+
+        WebElement frame_middle = driver.findElement(By.cssSelector("frame[name='frame-middle']"));  //Find middle frame
+        driver.switchTo().frame(frame_middle);  //Switch to middle frame
+
+        System.out.println(driver.findElement(By.id("content")).getText());
+
+    }
+}
---
+++ src/test/java/DragDrop.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class DragDrop {
+    public static void main(String[] args) {
+
+        //Initial Setup
+        WebDriver driver = new ChromeDriver();  //Open chrome driver
+        driver.manage().window().maximize();  //Maximize window
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));  //Implicit wait of 10s
+
+        //SCRIPT BEGINS FROM HERE--------------------------------------------------------
+        String url = "https://jqueryui.com/droppable/";
+        driver.get(url);
+
+        WebElement iframe = driver.findElement(By.cssSelector("iframe[class='demo-frame']"));  //Locate the iframe
+        //driver.switchTo().frame(0);  //Switch to iframe using index
+        driver.switchTo().frame(iframe);  //Switch to the iframe
+
+        WebElement source = driver.findElement(By.cssSelector("#draggable"));   //Find box top drag
+        WebElement destination = driver.findElement(By.cssSelector("#droppable"));  //Find box in which we drop
+
+        Actions action = new Actions(driver);   //Create actions class
+        action.dragAndDrop(source,destination).build().perform();  //Drag and drop the box
+
+        driver.switchTo().defaultContent();  //Get out of iframe
+
+        System.out.println(driver.findElement(By.cssSelector(".entry-title")).getText());  //Get h3 text to verify we are out of iframe
+
+    }
+}
