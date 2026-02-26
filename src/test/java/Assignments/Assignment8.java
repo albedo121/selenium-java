ab8b77269c764d00dc14e876ee25ed6e0542d003
albedo121 <vyzzal999@gmail.com>
2026-02-26 22:49:08 +0530

    Completed Assignment8

--- src/test/java/Assignments/Assignment8.java
+++ src/test/java/Assignments/Assignment8.java
@@ -1,12 +1,20 @@
+//In suggestion class example input box input kwd 'ind' and select India from suggestions. Verify that india is present in text bpx
+
 package Assignments;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Assignment8 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //Initial Setup
         WebDriver driver = new ChromeDriver();  //Invoke chrome browser
         driver.manage().window().maximize();  //Maximize window
@@ -16,6 +24,18 @@ public class Assignment8 {
         String url = "https://rahulshettyacademy.com/AutomationPractice/";
         driver.get(url);
 
+        //Find the 'Suggestion class example' text box and send keys ind
+        driver.findElement(By.id("autocomplete")).sendKeys("ind");
+
+        //Wait for suggestion menu to load
+        driver.findElement(By.cssSelector(".ui-menu-item-wrapper"));
+
+        //Press down arrow 2 times to select India
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+        driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+
+        //Get text from text box
+        System.out.println(driver.findElement(By.id("autocomplete")).getAttribute("value"));
 
     }
 }
--- src/test/java/scroll.java
+++ src/test/java/scroll.java
@@ -1,3 +1,4 @@
+//JS executor
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
